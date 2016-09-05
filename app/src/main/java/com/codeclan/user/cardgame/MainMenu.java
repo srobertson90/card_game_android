7ee1af63ebd59545bcadfd57aed4798029321654
srobertson90 <stuart.robertson.applications@gmail.com>
2016-09-05 13:33:40 +0100

    added main menu page

--- app/src/main/java/com/codeclan/user/cardgame/MainMenu.java
+++ app/src/main/java/com/codeclan/user/cardgame/MainMenu.java
@@ -1,11 +1,17 @@
 package com.codeclan.user.cardgame;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * Created by user on 05/09/2016.
  */
 public class MainMenu extends AppCompatActivity{
 
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
