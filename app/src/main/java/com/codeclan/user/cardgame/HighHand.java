e4076280076f9389aa7752870ea4def22e762663
srobertson90 <stuart.robertson.applications@gmail.com>
2016-09-05 18:00:19 +0100

    added highhand page and button to navigate

---
+++ app/src/main/java/com/codeclan/user/cardgame/HighHand.java
@@ -0,0 +1,16 @@
+package com.codeclan.user.cardgame;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 05/09/2016.
+ */
+public class HighHand extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_high_hand);
+    }
+}
--- app/src/main/java/com/codeclan/user/cardgame/MainMenu.java
+++ app/src/main/java/com/codeclan/user/cardgame/MainMenu.java
@@ -1,17 +1,32 @@
 package com.codeclan.user.cardgame;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+
 
 /**
  * Created by user on 05/09/2016.
  */
 public class MainMenu extends AppCompatActivity{
 
+    Button mHighHandButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mHighHandButton=(Button)findViewById(R.id.high_hand_button);
+
+        mHighHandButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainMenu.this, HighHand.class);
+                startActivity(intent);
+            }
+        });
     }
 }
