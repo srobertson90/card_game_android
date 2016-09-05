7a138c20906f45e3a4d460d3b8e57e14ad627762
srobertson90 <stuart.robertson.applications@gmail.com>
2016-09-05 19:30:51 +0100

    Added navigation back to menu

--- app/src/main/java/com/codeclan/user/cardgame/HighHand.java
+++ app/src/main/java/com/codeclan/user/cardgame/HighHand.java
@@ -1,16 +1,31 @@
 package com.codeclan.user.cardgame;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by user on 05/09/2016.
  */
 public class HighHand extends AppCompatActivity {
 
+    Button mMenuButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high_hand);
+
+        mMenuButton=(Button)findViewById(R.id.menu_button);
+
+        mMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HighHand.this, MainMenu.class);
+                startActivity(intent);
+            }
+        });
     }
 }
