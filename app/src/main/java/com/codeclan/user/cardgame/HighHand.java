c7c164a9254caa424fdac84716fd71da749768a0
srobertson90 <stuart.robertson.applications@gmail.com>
2016-09-07 10:58:27 +0100

    added text to high hand activity, now plays round

--- app/src/main/java/com/codeclan/user/cardgame/HighHand.java
+++ app/src/main/java/com/codeclan/user/cardgame/HighHand.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.codeclan.user.cardgame.cards.*;
+import com.codeclan.user.cardgame.game.*;
+import com.codeclan.user.cardgame.players.*;
 
 /**
  * Created by user on 05/09/2016.
@@ -12,6 +17,10 @@ import android.widget.Button;
 public class HighHand extends AppCompatActivity {
 
     Button mMenuButton;
+    Button mPlayButton;
+    TextView mPlayer1Score;
+    TextView mPlayer2Score;
+    TextView mWinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +28,30 @@ public class HighHand extends AppCompatActivity {
         setContentView(R.layout.activity_high_hand);
 
         mMenuButton=(Button)findViewById(R.id.menu_button);
+        mPlayButton=(Button)findViewById(R.id.play_button);
+        mPlayer1Score=(TextView)findViewById(R.id.player_1_score);
+        mPlayer2Score=(TextView)findViewById(R.id.player_2_score);
+        mWinner=(TextView)findViewById(R.id.winner);
 
-        mMenuButton.setOnClickListener(new View.OnClickListener() {
+        mMenuButton.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
+            public void onClick(View view){
                 Intent intent = new Intent(HighHand.this, MainMenu.class);
                 startActivity(intent);
+            }
+        });
+
+        mPlayButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Game game = new Game();
+                User player1 = new User();
+                Dealer player2 = new Dealer();
+                game.initPlayers(player1, player2);
+                game.initRound();
+                game.dealOut();
+                mPlayer1Score.setText(player1.handPrint());
+                mPlayer2Score.setText(player2.handPrint());
             }
         });
     }
